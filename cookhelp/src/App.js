7cacb1867f4af8220e17fe9414624e501119b2c2
김민석|Minseok Kim <alstjr7437@naver.com>
2023-03-31 12:43:33 +0900

    first project

---
+++ cookhelp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Auth } from './components/auth';
+// import { firestore } from "./config/firebase.js";
+
+function App() {
+  return (
+
+    <div class="App">
+      <Auth/>
+    </div>
+
+  );
+}
+
+export default App;
