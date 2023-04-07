152b81b13c53048b8b73d23bd873255f82bdce6c
sujeong <qkrtnwjd4212@naver.com>
2023-04-07 17:35:34 +0900

    feat: 페이지 초기설정

---
+++ cookhelp/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+import { styled } from '@mui/material/styles';
+import { Paper, Button } from "@mui/material"
+
+const Main = () => {
+  return <div>
+    <NavBar />
+  </div>;
+};
+
+export default Main;
