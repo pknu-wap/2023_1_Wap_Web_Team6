93476e032dd78f1f1e84c7b9e1240cb9ba57d064
김민석|Minseok Kim <alstjr7437@naver.com>
2023-05-01 15:10:41 +0900

    test

--- Backend/src/App.tsx
+++ Backend/src/App.tsx
@@ -1,19 +1,26 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from "./pages/Main";
-import Login from "./pages/Login";
-import Join from "./pages/Join";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (<div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/join" element={<Join />}/>
-      </Routes>
-    </BrowserRouter>
-  </div>);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
