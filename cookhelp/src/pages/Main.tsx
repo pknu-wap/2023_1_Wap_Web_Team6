56d3856b525069dcdf6c5d5f84d8be8b101a8f0c
sujeong <qkrtnwjd4212@naver.com>
2023-05-01 15:28:06 +0900

    style: 샘플 이미지 추가

--- cookhelp/src/pages/Main.tsx
+++ cookhelp/src/pages/Main.tsx
@@ -20,6 +20,7 @@ const CarouselItemImg = styled.img`
   width: 100%;
   //min-width: 400px;
   margin-bottom: -10px;
+  object-fit:cover;
 `;
 
 const MainCarousel = () => {
@@ -32,12 +33,12 @@ const MainCarousel = () => {
           <p>완전 건강해 보이는 요리의 레시피입니다. 닭가슴살과 아보카도, 방울토마토, 연어, 어쩌구</p>
         </Paper>
         <Paper>
-          <CarouselItemImg src="https://www.chickensaladchick.com/assets/mainstage/mainstage-img.jpg"/>
+          <CarouselItemImg src="https://www.chickensaladchick.com/assets/mainstage/mainstage-img.jpg" />
           <h2>치킨 샐러드 샌드위치</h2>
           <p>치킨 샐러드 샌드위치의 레시피입니다. 통밀 베이글에 양상추와 토마토, 어쩌구를 넣어서 </p>
         </Paper>
         <Paper>
-          <CarouselItemImg src="./temp.png"/>
+          <CarouselItemImg src="./temp.png" />
           <h2>Recipe 3</h2>
           <p>This is sample paragraph ! </p>
         </Paper>
@@ -50,35 +51,44 @@ const SlideWrapper = styled.div`
   display: flex;
   justify-content: center;
   width: 80%;
+  max-width: 1000px;
   margin: auto; 
   margin-top: 50px;
   gap: 20px;
 `;
 
+const CardWrapper = styled.div`
+  flex-basis: 0;
+  flex-grow: 1;
+`;
+
 // JSX에서는 props를 객체 형태로 전달하기 때문에 props의 타입도 객체 형태로 정의해줘야 함!!
-type FoodGenreProps = {
+type FoodGenreCardsProps = {
   genreName: string;
+  imgSrc: string;
 }
 
-function FoodGenre(props :FoodGenreProps) {
+function FoodGenreCards(props: FoodGenreCardsProps) {
   return (
+    <CardWrapper>
       <Card sx={{ maxWidth: 200 }}>
         <CardActionArea>
           <CardMedia
             component="img"
             height="140"
-            image="./temp.png"
+            image={props.imgSrc}
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {props.genreName}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles.
+              Sample Paragraph
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
+    </CardWrapper>
   );
 };
 
@@ -112,10 +122,10 @@ const Main = () => {
     <NavBar />
     <MainCarousel />
     <SlideWrapper>
-      <FoodGenre genreName = "한식" />
-      <FoodGenre genreName = "중식" />
-      <FoodGenre genreName = "일식" />
-      <FoodGenre genreName = "양식" />
+      <FoodGenreCards genreName="한식" imgSrc="./한식.jpg" />
+      <FoodGenreCards genreName="중식" imgSrc="./중식.jpg" />
+      <FoodGenreCards genreName="일식" imgSrc="./일식.jpg" />
+      <FoodGenreCards genreName="양식" imgSrc="./양식.jpg" />
     </SlideWrapper>
   </div>;
 };
