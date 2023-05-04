4f2aff287745d28478ca543a1eb701c3040964b7
sujeong <qkrtnwjd4212@naver.com>
2023-05-04 15:52:38 +0900

    feat: 샘플데이터, 타입정보 파일 분리

---
+++ cookhelp/code/src/tmpDB/tmpRecipeListDB.tsx
@@ -0,0 +1,30 @@
+import { searchData } from "../components/type";
+
+const searchTmpData: searchData[] = [
+    {
+        id: 0,
+        img: "https://cdn.pixabay.com/photo/2022/12/29/01/01/image-7683986_960_720.jpg",
+        title: "떡볶이",
+        writer: "작성자",
+        date: "2023/05/04",
+        views: 1534,
+    },
+    {
+        id: 1,
+        img: "https://cdn.pixabay.com/photo/2017/06/17/16/20/waffles-2412628_960_720.jpg",
+        title: "와플",
+        writer: "작성자",
+        date: "2023/05/05",
+        views: 14,
+    },
+    {
+        id: 2,
+        img: "https://cdn.pixabay.com/photo/2018/06/04/13/36/cold-noodles-3453218_960_720.jpg",
+        title: "냉면",
+        writer: "작성자",
+        date: "2023/05/06",
+        views: 579,
+    },
+];
+
+export default searchTmpData;
