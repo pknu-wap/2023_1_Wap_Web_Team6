eb475e53ae0f02f31ec4a06cac652c2e9a3d7706
sujeong <qkrtnwjd4212@naver.com>
2023-05-04 15:52:47 +0900

    feat: 샘플데이터, 타입파일 분리

--- cookhelp/code/src/pages/RecipeList.tsx
+++ cookhelp/code/src/pages/RecipeList.tsx
@@ -4,6 +4,9 @@ import Navbar from "../components/NavBar";
 import Container from "../UI/Container";
 import searchLogo from "../assets/searchLogo.png";
 import RecipeItem from "../components/RecipeItem";
+import { searchData } from "../components/type";
+import searchTmpData from "../tmpDB/tmpRecipeListDB";
+import Button from '@mui/material/Button';
 
 const RecipeListContainer = styled(Container)`
   max-width: 60rem;
@@ -32,45 +35,24 @@ const ListContainer = styled.div`
   width: 90%;
   height: 500px;
 `;
+const ListHeader = styled.div`
 
-// 나중에 type.ts 파일에 옮기기
-interface searchData {
-  id: number;
-  img: string;
-  title: string;
-  writer: string;
-  date: string;
-  views?: number;
-}
+`;
+const RegisterBtn = styled.button`
+  padding: 10px;
+  font-size: 16px;
+  background-color: var(--green-color);
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  margin: auto;
+
+  &:hover {
+    background-color: var(--dark-green-color);
+  }
+`;
 
 const RecipeList = () => {
-  const searchTmpData: searchData[] = [
-    {
-      id: 0,
-      img: "https://cdn.pixabay.com/photo/2022/12/29/01/01/image-7683986_960_720.jpg",
-      title: "떡볶이",
-      writer: "작성자",
-      date: "2023/05/04",
-      views: 1534,
-    },
-    {
-      id: 1,
-      img: "https://cdn.pixabay.com/photo/2017/06/17/16/20/waffles-2412628_960_720.jpg",
-      title: "와플",
-      writer: "작성자",
-      date: "2023/05/05",
-      views: 14,
-    },
-    {
-      id: 2,
-      img: "https://cdn.pixabay.com/photo/2018/06/04/13/36/cold-noodles-3453218_960_720.jpg",
-      title: "냉면",
-      writer: "작성자",
-      date: "2023/05/06",
-      views: 579,
-    },
-  ];
-
   return (
     <>
       <Navbar />
@@ -82,18 +64,21 @@ const RecipeList = () => {
           <SearchInput placeholder="레시피 검색"></SearchInput>
         </LogoBox>
 
+
         <ListContainer>
+          <ListHeader />
           {searchTmpData.map((ele: searchData) => {
-            return <RecipeItem 
-            RecipeId={ele.id}
-            RecipeTitle={ele.title}
-            RecipeImg={ele.img}
-            RecipeWriter={ele.writer}
-            RecipeDate={ele.date}
+            return <RecipeItem
+              RecipeId={ele.id}
+              RecipeTitle={ele.title}
+              RecipeImg={ele.img}
+              RecipeWriter={ele.writer}
+              RecipeDate={ele.date}
             />;
           })}
         </ListContainer>
       </RecipeListContainer>
+      <RegisterBtn>레시피 등록</RegisterBtn>
     </>
   );
 };
