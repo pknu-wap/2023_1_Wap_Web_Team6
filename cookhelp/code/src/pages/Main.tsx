152f5205de54447cad2fb265def0509000a72d0f
yunjikwak <mia007478@gmail.com>
2023-05-15 10:49:33 +0900

    feat: 컴포넌트 분리

--- cookhelp/code/src/pages/Main.tsx
+++ cookhelp/code/src/pages/Main.tsx
@@ -1,11 +1,7 @@
 import NavBar from "../components/NavBar";
 import styled from "styled-components";
 import MainCarousel from "../components/MainCarousel";
-import FoodGenreCards from "../components/FoodGenreCards";
-import KoreanFood from "../assets/한식.jpg";
-import ChineseFood from "../assets/중식.jpg";
-import WesternFood from "../assets/양식.jpg";
-import JapaneseFood from "../assets/일식.jpg";
+import RecommendCard from "../components/RecommendCard";
 
 // const CardSlide = () => {
 //   return (
@@ -32,27 +28,12 @@ import JapaneseFood from "../assets/일식.jpg";
 //   );
 // };
 
-const AllCardWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 80%;
-  max-width: 1000px;
-  margin: auto;
-  margin-top: 50px;
-  gap: 20px;
-`;
-
 const Main = () => {
   return (
     <div>
       <NavBar />
       <MainCarousel />
-      <AllCardWrapper>
-        <FoodGenreCards genreName="한식" imgSrc={KoreanFood} />
-        <FoodGenreCards genreName="중식" imgSrc={ChineseFood} />
-        <FoodGenreCards genreName="일식" imgSrc={JapaneseFood} />
-        <FoodGenreCards genreName="양식" imgSrc={WesternFood} />
-      </AllCardWrapper>
+      <RecommendCard />
     </div>
   );
 };
