a8e2cfb9507f05a09b4829b2bfd54fcffa4c045a
yunjikwak <mia007478@gmail.com>
2023-05-21 18:09:13 +0900

    feat: router 변경

--- cookhelp/code/src/App.tsx
+++ cookhelp/code/src/App.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, ReactNode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Recipe from "./pages/Recipe";
 import RecipeList from "./pages/RecipeList";
+import cardData from "./tmpDB/tmpRecipeDB";
+import { RecipeCard } from "./components/type";
 
 function App() {
+  const [deck, setDeck] = useState<RecipeCard[]>(cardData);
+
+  let content: ReactNode = <Route path="#" element={<></>} />;
+
+  if (deck.length > 0) {
+    content = deck.map((card, index) => {
+      // console.log(`/recipe/${index}`);
+      return (
+        <Route
+          key={card.id}
+          path={`/recipe/${index}`}
+          element={<Recipe key={card.id} cardData={deck[index]} />}
+        />
+      );
+    });
+  }
+
   return (
     <div>
       <BrowserRouter>
@@ -14,7 +33,8 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/join" element={<Join />} />
-          <Route path="/recipe" element={<Recipe />} />
+          {/* <Route path="/recipe" element={<Recipe />} /> */}
+          {content}
           <Route path="/recipe_list" element={<RecipeList />} />
         </Routes>
       </BrowserRouter>
