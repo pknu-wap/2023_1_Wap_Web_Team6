c234e46273519399ea39dc9d1c23d82a0b55e1fe
yunjikwak <mia007478@gmail.com>
2023-05-30 20:12:48 +0900

    feat:페이지네이션 추가

--- cookhelp/front/src/components/FoodGenreCards.tsx
+++ cookhelp/front/src/components/FoodGenreCards.tsx
@@ -1,14 +1,16 @@
-import { Card } from '@mui/material'
+import { Card } from "@mui/material";
 import styled from "styled-components";
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const CardWrapper = styled.div`
+const CardWrapper = styled(Link)`
   flex-basis: 0;
   flex-grow: 1;
   margin-bottom: 30px;
+  text-decoration-line: none;
 `;
 
 const CardTitle = styled.div`
@@ -19,18 +21,16 @@ const CardTitle = styled.div`
 type FoodGenreCardsProps = {
   genreName: string;
   imgSrc: string;
-}
+};
 
 function FoodGenreCards(props: FoodGenreCardsProps) {
   return (
-    <CardWrapper>
+    <CardWrapper
+      to={{ pathname: "/recipe_list", search: `?genreName=${props.genreName}` }}
+    >
       <Card sx={{ maxWidth: 200 }}>
         <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={props.imgSrc}
-          />
+          <CardMedia component="img" height="140" image={props.imgSrc} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               <CardTitle>{props.genreName}</CardTitle>
@@ -43,6 +43,6 @@ function FoodGenreCards(props: FoodGenreCardsProps) {
       </Card>
     </CardWrapper>
   );
-};
+}
 
 export default FoodGenreCards;
---
+++ cookhelp/front/src/pages/Mypage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/NavBar";
+import Container from "../UI/Container";
+
+const Mypage = () => {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <h1>Mypage</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Mypage;
