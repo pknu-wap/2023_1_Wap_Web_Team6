861bd7618fb57f94a907f4ea8125107e9c34d82b
김민석|Minseok Kim <alstjr7437@naver.com>
2023-06-07 13:51:19 +0900

    게시판 내용

--- cookhelp/front/src/pages/Board.tsx
+++ cookhelp/front/src/pages/Board.tsx
@@ -3,15 +3,14 @@ import styled from "styled-components";
 import NavBar from "../components/NavBar";
 import Slider from "../components/Slider";
 import { ListProps, DeckData, Card } from "../components/type";
-import RecommendCard from "../components/RecommendCard";
-import { Form, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Wrap = styled.div`
   margin-top: 2rem;
   display: flex;
   justify-content: center;
   gap: 1rem;
-
+  이태윤 존잘
   /* @media screen and (max-width: 800px) {
     margin-top: 2rem;
     flex-direction: column;
@@ -19,26 +18,35 @@ const Wrap = styled.div`
   } */
 `;
 
-const RightSideBar = styled.div`
-  background-color: var(--light-gray-color);
-
-  max-width: 250px;
-  width: 25%;
-  margin: 0;
-
-  /* @media screen and (max-width: 800px) {
-    max-width: 100%;
-    width: 600px;
-    max-height: 50%;
-    height: 150px;
-  } */
+const Post_view_wrapper = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  이태윤 존잘
 `;
-const LeftSideBar = styled.div`
-  background-color: var(--dark-green-color);
-  color: white;
-  width: 25%;
-  max-width: 250px;
+
+const Post_view_go_list_btn = styled.div`
+  border: 0;
+  padding: 10px;
+  background-color: #ffd9d9;
 `;
+
+const Post_view_row  = styled.div`
+  margin: 10px 0;
+  display: flex;
+`;
+
+const Post_view_row1 = styled.div`
+  margin: 10px 0;
+  width: 30%;
+  font-weight: bold;
+`;
+
+const Post_view_row2 = styled.div`
+  margin: 10px 0;
+  width: 70%;
+`;
+
+
 const ListTitle = styled.ol`
   font-weight: var(--Bold-font);
   font-size: 18px;
@@ -53,25 +61,7 @@ const StyledList = styled.li`
 `;
 const Main = styled.div`
   background-color: var(--mint-color);
-  width: 50%;
-`;
-const IngredientItem = styled.li`
-  font-size: 1rem;
-  margin-bottom: 5px;
-  list-style: none;
-  padding-left: 1rem;
-`;
-const Footer = styled.footer`
-  background-color: var(--green-color);
-  color: white;
-  margin: 0 auto;
-  margin-top: 2rem;
-  display: flex;
-  justify-content: center;
-
-  width: 1500px;
-  max-width: 90%;
-  height: 190px;
+  width: 80%;
 `;
 
 const List = ({ stepDetail, listNum, setSelectIdx }: ListProps) => {
@@ -91,36 +81,79 @@ const Recipe = () => {
   const params = useParams();
   // console.log(params.recipe_idx);
 
-  const [deck, setDeck] = useState();
+  const [board_idx, setboard_idx] = useState();
+  const [title, settitle] = useState();
+  const [content, setcontent] = useState();
+  const [members, setmembers] = useState();
+  const [boardstyle, setboardstyle] = useState();
+  const [create_date, setcreate_date] = useState();
   const [selectIdx, setSelectIdx] = useState<number>(1);
   const [Ingredient, setIngredient] = useState<string[]>([]);
   const [cards, setCards] = useState<Card[]>([]);
 
-  useEffect(() => {
-    const fetchRecipeHelper = async () => {
-      try {
-        const res = await fetch(
-          `http://localhost:8081/community/api/board/${params.board_idx}`
-        );
-        const data = await res.json();
 
-        if (!res.ok) {
-          console.log("error : ", data.description);
-          return;
+    useEffect(() => {
+      const fetchRecipeHelper = async () => {
+        try {
+          const res = await fetch(
+            `http://localhost:8081/community/api/board/${params.board_idx}`
+          );
+          const data = await res.json();
+
+          if (!res.ok) {
+            console.log("error : ", data.description);
+            return;
+          }
+          console.log("data : ", data[0]);
+          // console.log("data : ", data.result[0]);
+          setboard_idx(data[0].board_idx);
+          settitle(data[0].title);
+          setcontent(data[0].content);
+          setmembers(data[0].members);
+          setboardstyle(data[0].boardstyle);
+          setcreate_date(data[0].create_date);
+          setIngredient(data.recipeStuffArray);
+        } catch (error) {
+          console.log("Error!", error);
         }
-        console.log("data : ", data);
-        // console.log("data : ", data.result[0]);
-        setDeck(data.result[0]);
-        setIngredient(data.recipeStuffArray);
-      } catch (error) {
-        console.log("Error!", error);
-      }
-    };
+      };
 
-    fetchRecipeHelper();
-  }, []);
+      fetchRecipeHelper();
+    }, []);
   return (
     <div>
+    <NavBar />
+    <Wrap>
+      <Main>
+
+      <ListTitle>{board_idx} : {title}</ListTitle>
+      <Post_view_wrapper>
+              <Post_view_row>
+                <label>게시글 번호</label>
+                <label>{ board_idx }</label>
+              </Post_view_row>
+              <Post_view_row1>
+                <label>제목</label>
+                <label>{ title }</label>
+              </Post_view_row1>
+              <Post_view_row1>
+                <label>작성일</label>
+                <label>{ create_date }</label>
+              </Post_view_row1>
+              {/* <div className="post-view-row">
+                <label>조회수</label>
+                <label>{  }</label>
+              </div> */}
+              <Post_view_row2>
+                <label>내용</label>
+                {/* <div dangerouslySetInnerHTML={{ __html: content }}></div>; */}
+              </Post_view_row2>
+        <button className="post-view-go-list-btn">목록으로 돌아가기</button>
+      </Post_view_wrapper>
+          <Slider cards={cards} selectIdx={selectIdx} />{content} {members}
+          {/* {SliderContent} */}
+        </Main>
+      </Wrap>
     </div>
   );
 };
