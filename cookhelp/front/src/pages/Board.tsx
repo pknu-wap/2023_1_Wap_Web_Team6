a6f28565b7baf9231e3ec2d54027bf2f7849a08b
김민석|Minseok Kim <alstjr7437@naver.com>
2023-06-07 20:33:18 +0900

    게시판 수정 삭제 구현 완료
    게시판 수정 삭제 구현 완료
    로그인 체크도 해서 같은 아이디일시 수정 삭제 가능함

--- cookhelp/front/src/pages/Board.tsx
+++ cookhelp/front/src/pages/Board.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import NavBar from "../components/NavBar";
-import Slider from "../components/Slider";
-import { ListProps, DeckData, Card } from "../components/type";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Wrap = styled.div`
   margin-top: 2rem;
   display: flex;
   justify-content: center;
   gap: 1rem;
-  이태윤 존잘
   /* @media screen and (max-width: 800px) {
     margin-top: 2rem;
     flex-direction: column;
@@ -18,79 +17,133 @@ const Wrap = styled.div`
   } */
 `;
 
-const Post_view_wrapper = styled.div`
-  width: 60%;
+const PostWrapper = styled.div`
+  width: 80%;
   margin: 0 auto;
-  이태윤 존잘
 `;
 
-const Post_view_go_list_btn = styled.div`
-  border: 0;
-  padding: 10px;
-  background-color: #ffd9d9;
-`;
-
-const Post_view_row  = styled.div`
+const PostView  = styled.div`
   margin: 10px 0;
   display: flex;
 `;
 
-const Post_view_row1 = styled.div`
+const ContentView  = styled.div`
   margin: 10px 0;
-  width: 30%;
+  display: flex;
+  border-top:3px solid white;
+`;
+
+const PostView1 = styled.label`
+  margin: 10px 0;
+  width: 20%;
   font-weight: bold;
 `;
 
-const Post_view_row2 = styled.div`
+const PostView2 = styled.label`
   margin: 10px 0;
-  width: 70%;
+  width: 80%;
 `;
 
-
-const ListTitle = styled.ol`
-  font-weight: var(--Bold-font);
-  font-size: 18px;
-  margin: 1rem 1rem;
-`;
-const StyledList = styled.li`
-  font-size: 1rem;
-  margin-bottom: 5px;
-  list-style-type: decimal;
+const ModifyButton = styled.button`
   cursor: pointer;
-  /* padding-left: 1rem; */
+  background-color: var(--green-color);
+  color: #fff;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 18px;
+  margin-left: 65%;
 `;
+
+const DeleteButton = styled.button`
+  cursor: pointer;
+  background-color:  var(--green-color);
+  color: #fff;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 18px;
+  margin-left: 1%;
+`;
+
+const ListButton = styled.button`
+  cursor: pointer;
+  background-color:  var(--green-color);
+  color: #fff;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 18px;
+  margin-left: 1%;
+`;
+
 const Main = styled.div`
   background-color: var(--mint-color);
   width: 80%;
 `;
 
-const List = ({ stepDetail, listNum, setSelectIdx }: ListProps) => {
-  const hadleListItem = (e: React.MouseEvent<HTMLLIElement>) => {
-    console.log(e.currentTarget.value);
-    setSelectIdx(e.currentTarget.value);
-  };
-
-  return (
-    <StyledList key={listNum} value={listNum} onClick={hadleListItem}>
-      {stepDetail}
-    </StyledList>
-  );
-};
-
 const Recipe = () => {
   const params = useParams();
   // console.log(params.recipe_idx);
 
-  const [board_idx, setboard_idx] = useState();
+  const [registerData, setRegisterData] = useState({
+    members: localStorage.getItem('loginId'),
+    board_idx: params.board_idx
+  });
+
+  const [board_idx, setboard_idx] = useState<string>("");
   const [title, settitle] = useState();
-  const [content, setcontent] = useState();
-  const [members, setmembers] = useState();
+  const [content, setcontent] = useState<string>("")
+  const [members, setmembers] = useState<string>("")
   const [boardstyle, setboardstyle] = useState();
   const [create_date, setcreate_date] = useState();
-  const [selectIdx, setSelectIdx] = useState<number>(1);
-  const [Ingredient, setIngredient] = useState<string[]>([]);
-  const [cards, setCards] = useState<Card[]>([]);
+  const navigate = useNavigate();
 
+  const deleteBtnClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault(); // 버튼 클릭의 기본 동작 중지
+    fetch("http://localhost:8081/community/api/boardDelete", {
+      method: "post", // method :통신방법
+      headers: {
+        // headers: API 응답에 대한 정보를 담음
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(registerData), //join 객체를 보냄
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.isSuccess === "True") {
+          alert("게시글 삭제 완료!");
+          console.log("게시글 삭제 완료!");
+          navigate("/board_list");
+        } else {
+          alert(data.isSuccess);
+        }
+      }).catch(function(err) {
+        console.error(` Err: ${err}`);
+    })
+  };
+
+  const modifyBtnClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault(); // 버튼 클릭의 기본 동작 중지
+    fetch("http://localhost:8081/community/api/infoCheck", {
+      method: "post", // method :통신방법
+      headers: {
+        // headers: API 응답에 대한 정보를 담음
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(registerData), //join 객체를 보냄
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.isSuccess === "True") {
+          navigate("/board_modify", {state:board_idx})
+        } else {
+          alert(data.isSuccess);
+        }
+      }).catch(function(err) {
+        console.error(` Err: ${err}`);
+    })
+  };
 
     useEffect(() => {
       const fetchRecipeHelper = async () => {
@@ -104,7 +157,6 @@ const Recipe = () => {
             console.log("error : ", data.description);
             return;
           }
-          console.log("data : ", data[0]);
           // console.log("data : ", data.result[0]);
           setboard_idx(data[0].board_idx);
           settitle(data[0].title);
@@ -112,7 +164,6 @@ const Recipe = () => {
           setmembers(data[0].members);
           setboardstyle(data[0].boardstyle);
           setcreate_date(data[0].create_date);
-          setIngredient(data.recipeStuffArray);
         } catch (error) {
           console.log("Error!", error);
         }
@@ -126,32 +177,39 @@ const Recipe = () => {
     <Wrap>
       <Main>
 
-      <ListTitle>{board_idx} : {title}</ListTitle>
-      <Post_view_wrapper>
-              <Post_view_row>
-                <label>게시글 번호</label>
-                <label>{ board_idx }</label>
-              </Post_view_row>
-              <Post_view_row1>
-                <label>제목</label>
-                <label>{ title }</label>
-              </Post_view_row1>
-              <Post_view_row1>
-                <label>작성일</label>
-                <label>{ create_date }</label>
-              </Post_view_row1>
+      <PostWrapper>
+              <PostView>
+                <PostView1>게시글 번호</PostView1>
+                <PostView2>{ board_idx }</PostView2>
+              </PostView>
+              <PostView>
+                <PostView1>게시글 유형</PostView1>
+                <PostView2>{ boardstyle }</PostView2>
+              </PostView>
+              <PostView>
+                <PostView1>제목</PostView1>
+                <PostView2>{ title }</PostView2>
+              </PostView>
+              <PostView>
+                <PostView1>작성한 사람</PostView1>
+                <PostView2>{ members }</PostView2>
+              </PostView>
+              <PostView>
+                <PostView1>작성일</PostView1>
+                <PostView2>{ create_date }</PostView2>
+              </PostView>
               {/* <div className="post-view-row">
                 <label>조회수</label>
                 <label>{  }</label>
               </div> */}
-              <Post_view_row2>
-                <label>내용</label>
-                {/* <div dangerouslySetInnerHTML={{ __html: content }}></div>; */}
-              </Post_view_row2>
-        <button className="post-view-go-list-btn">목록으로 돌아가기</button>
-      </Post_view_wrapper>
-          <Slider cards={cards} selectIdx={selectIdx} />{content} {members}
-          {/* {SliderContent} */}
+              <ContentView>
+                <PostView1>내용</PostView1>
+                <div dangerouslySetInnerHTML={{ __html: content }}></div>
+              </ContentView>
+      </PostWrapper>
+        <ModifyButton onClick={modifyBtnClick}>게시글 수정</ModifyButton>
+        <DeleteButton onClick={deleteBtnClick}>게시글 삭제</DeleteButton>
+        <ListButton onClick={() => navigate("/board_list")}>목록 돌아가기</ListButton>
         </Main>
       </Wrap>
     </div>
