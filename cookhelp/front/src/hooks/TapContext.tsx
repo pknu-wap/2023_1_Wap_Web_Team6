aff0bfba41316c6c5e5dcd39653531b61b44edb8
sujeong <qkrtnwjd4212@naver.com>
2023-06-09 17:38:37 +0900

    feat:오류 수정 및 css 수정

--- cookhelp/front/src/hooks/TapContext.tsx
+++ cookhelp/front/src/hooks/TapContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState,useEffect } from "react";
 
 type TapContextValue = {
   tapItem: number;
@@ -13,8 +13,16 @@ export const TapContext = createContext<TapContextValue>({
 export const TapProvider = ({ children }: { children: React.ReactNode }) => {
   const [tapItem, setTapItem] = useState<number>(0);
 
+  const curValue = localStorage.getItem('curTap');
+
+  useEffect (() => {
+    if (curValue === "recipe") setTapItem(1)
+    else if (curValue === "community") setTapItem(2)
+    else if (curValue === "") setTapItem(0)
+  }, [])
+
   return (
-    <TapContext.Provider value={{ tapItem, setTapItem }}>
+    <TapContext.Provider value={{ tapItem, setTapItem}}>
       {children}
     </TapContext.Provider>
   );
